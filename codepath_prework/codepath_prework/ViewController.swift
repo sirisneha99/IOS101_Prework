62666974ddfb0def71a5051fa56c19bb0bae477f
Sneha <155668049+sirisneha99@users.noreply.github.com>
2025-05-01 02:59:27 -0400

    Adding Xcode project files

---
+++ codepath_prework/codepath_prework/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  codepath_prework
+//
+//  Created by sneha siri nagabathula on 5/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var changeBackgroundColor: UIButton!
+    
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    @IBAction func changeBackgroundColorTapped(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
